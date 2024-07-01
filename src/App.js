a2dd5ca244039edb48f7504ff8617fe94f96e77d
lautarotetamusa <lautarotetamusa@gmail.com>
2024-07-01 09:55:47 -0300

    No tengo ni idea de React, código completamente hecho con chat-gpt. Es una mierda? si, funciona? también

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { ActionForm } from './components/ActionForm';
+import { DataTable  } from './components/Table';
 import {actions} from './config';
 
-
+/*
 function App() {
   return (
     <div className="App">
       <h1>Action Form</h1>
       <ActionForm actions={actions} />
+    </div>
+  );
+}
+*/
+function App() {
+  return (
+    <div className="App">
+      <h1>Lista</h1>
+        <DataTable />
     </div>
   );
 }
